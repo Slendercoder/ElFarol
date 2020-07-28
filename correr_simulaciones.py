266de71c93de0815740c15e911b2f198b271ae46
Slendercoder <andradeed78@gmail.com>
2020-07-28 11:36:31 -0500

    OK

--- correr_simulaciones.py
+++ correr_simulaciones.py
@@ -2,7 +2,7 @@ import pandas as pd
 import ElFarolFunciones as F
 import redes1
 
-def crea_dataframe_agentes(Num_agentes, tipoRed, Agentes, Num_iteraciones, PARAMETROS, N):
+def crea_dataframe_agentes(Num_agentes, tipoRed, Agentes, Num_iteraciones, PARAMETROS, N,corte=10):
     muestra = []
     num_agentes = []
     red = []
@@ -17,17 +17,18 @@ def crea_dataframe_agentes(Num_agentes, tipoRed, Agentes, Num_iteraciones, PARAM
         lista_parametros.append([])
     for i in range(len(Agentes)):
         for r in range(Num_iteraciones):
-            muestra.append(N)
-            num_agentes.append(Num_agentes)
-            red.append(tipoRed)
-            agente.append(i)
-            ronda.append(r)
-            estado.append(Agentes[i].estado[r])
-            puntaje.append(Agentes[i].score[r])
-            politica.append(Agentes[i].politica[r])
-            lista_num_iteraciones.append(Num_iteraciones)
-            for x in range(len(PARAMETROS)):
-                lista_parametros[x].append(PARAMETROS[x])
+            if r%corte == 0:
+                muestra.append(N)
+                num_agentes.append(Num_agentes)
+                red.append(tipoRed)
+                agente.append(i)
+                ronda.append(r)
+                estado.append(Agentes[i].estado[r])
+                puntaje.append(Agentes[i].score[r])
+                politica.append(Agentes[i].politica[r])
+                lista_num_iteraciones.append(Num_iteraciones)
+                for x in range(len(PARAMETROS)):
+                    lista_parametros[x].append(PARAMETROS[x])
     data = pd.DataFrame.from_dict(\
     {\
     'Identificador': muestra,\
@@ -55,9 +56,9 @@ def simulacion(Num_agentes, tipoRed, Num_iteraciones, UMBRAL, inicial, N, PARS):
     data = crea_dataframe_agentes(Num_agentes, tipoRed, agentes, Num_iteraciones, PARS, N)
     data['Politica_lag'] = data.groupby('Agente')['Politica'].transform('shift', 1)
     data['Consistencia'] = data.apply(lambda x : F.encontrar_consistencia (x['Politica'], x['Politica_lag']), axis=1)
-    F.guardar(data, 'simulaciones.csv', inicial)
+    F.guardar(data, 'simulaciones'+tipoRed+str(PARS[1])+'.csv', inicial)
 
-Num_iteraciones = 300
+Num_iteraciones = 100
 identificador = 0
 UMBRAL = 0.5
 inicial = True
@@ -76,7 +77,7 @@ print('Corriendo simulacion red 2-regular...')
 tipoRed = 'Kregular'
 for Num_agentes in [5,6,10,11,101,1000]:
     for i in range(100):
-        PARS = [Num_agentes, 1]
+        PARS = [Num_agentes, 2]
         redes1.create_graph(Num_agentes, tipoRed, 2, True)
         simulacion(Num_agentes,tipoRed,Num_iteraciones,UMBRAL,inicial,identificador,PARS)
         identificador += 1
@@ -85,7 +86,7 @@ print('Corriendo simulacion red 4-regular...')
 tipoRed = 'Kregular'
 for Num_agentes in [5,6,10,11,101,1000]:
     for i in range(100):
-        PARS = [Num_agentes, 1]
+        PARS = [Num_agentes, 4]
         redes1.create_graph(Num_agentes, tipoRed, 4, True)
         simulacion(Num_agentes,tipoRed,Num_iteraciones,UMBRAL,inicial,identificador,PARS)
         identificador += 1
