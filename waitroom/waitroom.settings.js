85ccfa5e6e555924a7b6a487ecee64fbeccc6151
Slendercoder <edgar@Edgars-Mac-mini.local>
2021-03-16 14:51:10 -0500

    Incluye consentimiento informado

--- waitroom/waitroom.settings.js
+++ waitroom/waitroom.settings.js
@@ -32,7 +32,7 @@ module.exports = {
      *
      * The size of each group dispatched by the waiting room
      */
-    GROUP_SIZE: 2,
+    GROUP_SIZE: 5,
 
     /**
      * ## POOL_SIZE (number) Optional
